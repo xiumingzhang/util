e56f63c6c191934ebb39eec7b7359eec2a11ea92
xiuming <xiuming@mit.edu>
2017-03-31 20:13:34 -0400

    edits

---
+++ toMP4.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Exactly two arguments required!"
+    echo "    1. Single-quoted regex (e.g., '*.mp4', '*_azimuth0.png')"
+    echo "    2. Frame rate"
+    exit 1
+fi
+regex=$1
+fps=$2
+
+ffmpeg -framerate ${fps} -pattern_type glob -i "${regex}" -vf scale=-2:720 -pix_fmt yuv420p video.mp4
+
+echo "Successful!"
+echo -e "All images matching ${regex} are compiled into `pwd`/video.mp4"
