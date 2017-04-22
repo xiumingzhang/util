d3650aa1a8ee111873024cac414c8b478e52aa1a
Xiuming Zhang <xiuming6zhang@gmail.com>
2017-04-22 01:27:10 -0400

    edits

--- py/xiuminglib/decorators.py
+++ py/xiuminglib/decorators.py
@@ -1,4 +1,12 @@
+#! /usr/bin/env python
+
 import time
+import os
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+thisFile = os.path.abspath(__file__)
 
 
 def timeIt(someFunc):
@@ -10,15 +18,37 @@ def timeIt(someFunc):
         results = someFunc(*arg, **kwargs)
         t = time.time() - t0
         funcName = someFunc.__name__
-        print("Function %s takes %.2f seconds" % (funcName, t))
+        logging.info("%s: Function %s takes %.2f seconds" % (thisFile, funcName, t))
         return results
+    return wrapper
+
+
+def existOK(makedirs):
+    """
+    Implements the exist_ok flag in >= Python 3.2, which avoids race conditions,
+    where one parallel worker checks the folder doesn't exist and wants to create
+    it with another worker doing so faster
+    """
+    def wrapper(*args, **kwargs):
+        try:
+            makedirs(*args, **kwargs)
+        except OSError, e:
+            if e.errno != 17:
+                raise
+            logging.debug("%s: %s already exists, but that is OK" % (thisFile, args[0]))
     return wrapper
 
 
 # Tests
 if __name__ == '__main__':
+    # timeIt
     @timeIt
     def findSums(x, y, z):
-        time.sleep(2)
+        time.sleep(1)
         return x + y, x + z, y + z, x + y + z
     print(findSums(1, 2, 3))
+    # existOK
+    newDir = os.path.join(os.path.dirname(__file__), 'test')
+    makedirs = existOK(os.makedirs)
+    makedirs(newDir)
+    makedirs(newDir)
