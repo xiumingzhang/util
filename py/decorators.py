3f1b81539562848f1bc3e8630cf950c63e6f8563
Xiuming Zhang <xiuming6zhang@gmail.com>
2017-06-05 15:29:17 -0400

    restructures

--- py/decorators.py
+++ py/decorators.py
@@ -1,29 +1,26 @@
 import time
 import os
+
 import logging
-
-
 logging.basicConfig(level=logging.INFO)
 thisFile = os.path.abspath(__file__)
 
 
-def timeIt(someFunc):
+def timeit(somefunc):
     """
     Outputs the time a function takes to execute
     """
     def wrapper(*arg, **kwargs):
         t0 = time.time()
-        results = someFunc(*arg, **kwargs)
+        results = somefunc(*arg, **kwargs)
         t = time.time() - t0
-        funcName = someFunc.__name__
-        logging.info(
-            "%s: Function %s takes %.2f seconds", thisFile, funcName, t
-        )
+        funcname = somefunc.__name__
+        logging.info("%s: Function %s takes %.2f seconds", thisFile, funcname, t)
         return results
     return wrapper
 
 
-def existOK(makedirsFunc):
+def existok(makedirs_func):
     """
     Implements the exist_ok flag in >= 3.2, which avoids race conditions,
     where one parallel worker checks the folder doesn't exist and wants to
@@ -31,26 +28,24 @@ def existOK(makedirsFunc):
     """
     def wrapper(*args, **kwargs):
         try:
-            makedirsFunc(*args, **kwargs)
+            makedirs_func(*args, **kwargs)
         except OSError as e:
             if e.errno != 17:
                 raise
-            logging.debug(
-                "%s: %s already exists, but that is OK", thisFile, args[0]
-            )
+            logging.debug("%s: %s already exists, but that is OK", thisFile, args[0])
     return wrapper
 
 
 # Tests
 if __name__ == '__main__':
-    # timeIt
-    @timeIt
-    def findSums(x, y, z):
+    # timeit
+    @timeit
+    def findsums(x, y, z):
         time.sleep(1)
         return x + y, x + z, y + z, x + y + z
-    print(findSums(1, 2, 3))
+    print(findsums(1, 2, 3))
     # existOK
     newDir = os.path.join(os.path.dirname(__file__), 'test')
-    makedirs = existOK(os.makedirs)
+    makedirs = existok(os.makedirs)
     makedirs(newDir)
     makedirs(newDir)
---
+++ py/extract_frames.py
@@ -0,0 +1,42 @@
+import argparse
+from os import makedirs
+from os.path import exists, join, abspath
+import logging
+from shutil import rmtree
+from cv2 import imwrite, VideoCapture
+
+logging.basicConfig(level=logging.INFO)
+thisfile = abspath(__file__)
+
+# Parse variables
+parser = argparse.ArgumentParser(description="Extract frames from a video file")
+parser.add_argument('videopath', metavar='v', type=str, help="video file")
+parser.add_argument('outdir', metavar='o', type=str, help="output directory")
+parser.add_argument('--every', metavar='n', type=int, default=1, help="sample one frame every n frame(s) (default: 1)")
+parser.add_argument('--outlen', metavar='l', type=int, default=4, help="length of output filenames (default: 4)")
+args = parser.parse_args()
+videopath = args.videopath
+outdir = abspath(args.outdir)
+every = args.every
+outlen = args.outlen
+
+# Make directory
+if exists(outdir):
+    rmtree(outdir)
+makedirs(outdir)
+
+# Read frames from video
+vid = VideoCapture(videopath)
+frameidx = 0
+frameidx_out = 1
+while vid.isOpened():
+    success, im = vid.read()
+    if not success:
+        break
+    if frameidx % every == 0:
+        outpath = join(outdir, str(frameidx_out).zfill(outlen) + '.png')
+        logging.info("%s: Frame %d saved as %s", thisfile, frameidx, outpath)
+        imwrite('%s' % outpath, im)
+        frameidx_out += 1
+    frameidx += 1
+vid.release()
