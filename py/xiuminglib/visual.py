95cef27af7072f96049df7d53e7e27db3d865852
Xiuming Zhang <xiuming6zhang@gmail.com>
2017-06-29 21:14:30 -0400

    edits

--- py/xiuminglib/visual.py
+++ py/xiuminglib/visual.py
@@ -9,7 +9,7 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
+from matplotlib.collections import LineCollection
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import cv2
 
@@ -27,9 +27,6 @@ def matrix_as_heatmap(mat, outpath='./matrix_as_heatmap.png', figtitle=None):
         figtitle: Figure title
             String
             Optional; defaults to None (no title)
-
-    Returns:
-        None
     """
     figsize = 14
     plt.figure(figsize=(figsize, figsize))
@@ -45,14 +42,14 @@ def matrix_as_heatmap(mat, outpath='./matrix_as_heatmap.png', figtitle=None):
     # Colorbar
     # Create an axes on the right side of ax. The width of cax will be 2%
     # of ax and the padding between cax and ax will be fixed at 0.1 inch.
-    cax = make_axes_locatable(ax).append_axes('right', size='2%', pad=0.1)
+    cax = make_axes_locatable(ax).append_axes('right', size='2%', pad=0.2)
     plt.colorbar(im, cax=cax)
 
     # Save plot
     plt.savefig(outpath, bbox_inches='tight')
 
 
-def verts_on_texmap(u, v, texmap, outpath='./verts_on_texmap.png', figtitle=None):
+def uv_on_texmap(u, v, texmap, ft=None, outpath='./uv_on_texmap.png', figtitle=None):
     """
     Visualizes which points on texture map the vertices map to
 
@@ -69,25 +66,27 @@ def verts_on_texmap(u, v, texmap, outpath='./verts_on_texmap.png', figtitle=None
             +-----------> (1, 0)
         (0, 0)        u
 
-        texmap: loaded texture map or its path
+        texmap: Loaded texture map or its path
             h-by-w (grayscale) or h-by-w-by-3 (color) numpy array or string
+        ft: Texture faces
+            List of lists of integers starting from 1, e.g., '[[1, 2, 3], [], [2, 3, 4, 5], ...]'
+            Optional; defaults to None. If provided, use it to connect UV points
         outpath: Path to which the visualization is saved
             String
-            Optional; defaults to './verts_on_texmap.png'
+            Optional; defaults to './uv_on_texmap.png'
         figtitle: Figure title
             String
             Optional; defaults to None (no title)
-
-    Returns:
-        None
     """
     figsize = 50
-    c = 'r'
-    s = 2
+    dc = 'r' # color
+    ds = 4 # size of UV dots
+    lc = 'b' # color
+    lw = 1 # width of edges connecting UV dots
 
     fig = plt.figure(figsize=(figsize, figsize))
     if figtitle is not None:
-        fig.suptitle(figtitle)
+        fig.title(figtitle)
 
     # Preprocess input
     if isinstance(texmap, str):
@@ -108,21 +107,35 @@ def verts_on_texmap(u, v, texmap, outpath='./verts_on_texmap.png', figtitle=None
     #   |
     #   v y
 
-    # Figure 1: scatter-plot UV coordinates
-    ax = fig.add_subplot(211)
-    ax.set_xlim([0, w])
-    ax.set_ylim([0, h])
-    ax.invert_yaxis() # origin left top corner
-    ax.scatter(x, y, c=c, s=s)
+    # UV dots
+    ax = fig.gca()
+    ax.set_xlim([min(0, min(x)), max(w, max(x))])
+    ax.set_ylim([max(h, max(y)), min(0, min(y))])
+    im = ax.imshow(texmap, cmap='gray')
+    ax.scatter(x, y, c=dc, s=ds)
     ax.set_aspect('equal')
 
-    # Figure 2: selection mask over texture image
-    ax = fig.add_subplot(212)
-    ax.set_xlim([0, w])
-    ax.set_ylim([h, 0])
-    ax.imshow(texmap)
-    ax.scatter(x, y, c=c, s=s) # origin left top corner
-    ax.set_aspect('equal')
+    # Also connect these dots
+    if ft is not None:
+        lines = []
+        for vert_id in ft:
+            if len(vert_id) > 0:
+                # For each face
+                ind = [i - 1 for i in vert_id]
+                n_verts = len(ind)
+                for i in range(n_verts):
+                    lines.append([
+                        (x[ind[i]], y[ind[i]]), # starting point
+                        (x[ind[(i + 1) % n_verts]], y[ind[(i + 1) % n_verts]]) # ending point
+                    ])
+        line_collection = LineCollection(lines, linewidths=lw, colors=lc)
+        ax.add_collection(line_collection)
+
+    # Colorbar
+    # Create an axes on the right side of ax. The width of cax will be 2%
+    # of ax and the padding between cax and ax will be fixed at 0.1 inch.
+    cax = make_axes_locatable(ax).append_axes('right', size='2%', pad=0.2)
+    plt.colorbar(im, cax=cax)
 
     # Save plot
     plt.savefig(outpath, bbox_inches='tight')
