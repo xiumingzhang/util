d381f71366c3ab722e8572460d133910f59b1bf0
Xiuming Zhang <xiuming6zhang@gmail.com>
2017-07-18 23:56:54 -0400

    enhances spherical-cartesian conversions

--- py/xiuminglib/geometry.py
+++ py/xiuminglib/geometry.py
@@ -22,17 +22,22 @@ def cartesian2spherical(pts_cartesian):
                           x     | (lat = -90)
 
     Args:
-        pts_cartesian: n-by-3 numpy array
-            Each row consists of x, y and z, in order.
+        pts_cartesian: Cartesian x, y and z
+            List/tuple/etc. that can be converted to a numpy array of shape '(3,)' or '(n, 3)'
 
     Returns:
-        pts_spherical: n-by-3 numpy array
-            Each row consists of r, lat and lng (in radians), in order.
+        pts_spherical: Spherical r, lat and lng (in radians)
+            Numpy array of same shape as input
     """
+    pts_cartesian = np.array(pts_cartesian)
 
     # Validate inputs
-    assert (len(pts_cartesian.shape) == 2), "'pts' must be a 2D array"
-    assert (pts_cartesian.shape[1] == 3), "Second dimension of 'pts' must be 3"
+    is_one_point = False
+    if pts_cartesian.shape == (3,):
+        is_one_point = True
+        pts_cartesian = pts_cartesian.reshape(1, 3)
+    elif len(pts_cartesian.shape) != 2 or pts_cartesian.shape[1] != 3:
+        raise ValueError("Shape of input must be either (3,) or (n, 3)")
 
     # Compute r
     r = np.sqrt(np.sum(np.square(pts_cartesian), axis=1))
@@ -46,8 +51,12 @@ def cartesian2spherical(pts_cartesian):
     y = pts_cartesian[:, 1]
     lng = np.arctan2(y, x) # choosing the quadrant correctly
 
-    # Assemble and return
+    # Assemble
     pts_spherical = np.stack((r, lat, lng), axis=-1) # let this new axis be the last dimension
+
+    if is_one_point:
+        pts_spherical = pts_spherical.reshape(3)
+
     return pts_spherical
 
 
@@ -57,10 +66,17 @@ def spherical2cartesian(pts_spherical):
 
     See cartesian2spherical for spherical convention, args and returns
     """
+    pts_spherical = np.array(pts_spherical)
 
     # Validate inputs
-    assert (len(pts_spherical.shape) == 2), "'pts' must be a 2D array"
-    assert (pts_spherical.shape[1] == 3), "Second dimension of 'pts' must be 3"
+    is_one_point = False
+    if pts_spherical.shape == (3,):
+        is_one_point = True
+        pts_spherical = pts_spherical.reshape(1, 3)
+    elif len(pts_spherical.shape) != 2 or pts_spherical.shape[1] != 3:
+        raise ValueError("Shape of input must be either (3,) or (n, 3)")
+
+    # Degrees?
     if (np.abs(pts_spherical) > 2 * np.pi).any():
         warn("Some input value falls outside [-2pi, 2pi]. Are you sure inputs are in radians?")
 
@@ -74,6 +90,10 @@ def spherical2cartesian(pts_spherical):
 
     # Assemble and return
     pts_cartesian = np.stack((x, y, z), axis=-1) # let this new axis be the last dimension
+
+    if is_one_point:
+        pts_cartesian = pts_cartesian.reshape(3)
+
     return pts_cartesian
 
 
