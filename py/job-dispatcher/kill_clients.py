169ec2b42b24af9368575dfb69cc500044f5ac6f
Xiuming Zhang <xiuming6zhang@gmail.com>
2019-02-24 23:04:12 -0500

    still some processes can't be killed, but seems ok. these processes may not be killed at all. but why are they included?

--- py/job-dispatcher/kill_clients.py
+++ py/job-dispatcher/kill_clients.py
@@ -37,7 +37,7 @@ def get_pids(machine_name, user, job_name):
 def _kill(machine_name, pid):
     cmd = ('ssh -o PasswordAuthentication=no -o LogLevel=QUIET -t %s '
            'kill -9 %s') % (machine_name, pid)
-    child = Popen(split(cmd))
+    child = Popen(split(cmd), stdout=DEVNULL, stderr=DEVNULL)
     _, _ = child.communicate()
     return child.returncode == 0
 
