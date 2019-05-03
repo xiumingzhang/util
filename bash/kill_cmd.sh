eafda4a5f7211160cb129d1fd1072093794bcade
Xiuming Zhang <xiuming6zhang@gmail.com>
2019-05-02 21:44:57 -0400

    simplies

--- bash/kill_cmd.sh
+++ bash/kill_cmd.sh
@@ -6,20 +6,17 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-cpu_list=( 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 )
-gpu_list=( 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 18 19 20 )
-
 user=$(whoami)
 
 # CPU
-for id in "${cpu_list[@]}"; do
+for id in {1..38}; do
     printf -v m "vision%02d" "$id"
     ssh "$user@$m.csail.mit.edu" "pkill -9 -f \"$1\"" &
     echo "$1: killed on $m"
 done
 
 # GPU
-for id in "${gpu_list[@]}"; do
+for id in {2..43}; do
     printf -v m "visiongpu%02d" "$id"
     ssh "$user@$m.csail.mit.edu" "pkill -9 -f \"$1\"" &
     echo "$1: killed on $m"
